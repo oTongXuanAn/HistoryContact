c39e069aafadfdd554cf83d55c6d021f621daf01
oTongXuanAn <tong.xuan.an@framgia.com>
2018-10-10 18:25:24 +0700

    chang request

--- app/src/main/java/an/xuan/tong/historycontact/api/ApiService.kt
+++ app/src/main/java/an/xuan/tong/historycontact/api/ApiService.kt
@@ -1,9 +1,11 @@
 package an.xuan.tong.historycontact.api
 
+import an.xuan.tong.historycontact.api.model.Account
 import an.xuan.tong.historycontact.api.model.TokenReponse
 import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiService {
@@ -11,4 +13,16 @@ interface ApiService {
     @GET("api/account/active")
     fun getInfomation(@Query("api") api: String,
                       @Query("phone") phone: String): Observable<TokenReponse>
+
+    //api/location/insert?api={api}
+    @POST("api/location/insert")
+    fun insertLocation(@Query("id") id: Int,
+                       @Query("idaccount") idaccount: Int,
+                       @Query("datecreate") datecreate: Int,
+                       @Query("lat") lat: Long,
+                       @Query("lng") lng: Long,
+                       @Query("account") account: Account)
+
+    //http://api.naxuto.com/Help/Api/POST-api-location-insert_api
+
 }
